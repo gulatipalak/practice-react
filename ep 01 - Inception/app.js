62f5a2568cb945193a8b8cc72b703f1ec3f447b4
gulatipalak <gulatipalak2397@gmail.com>
2025-02-06 14:14:04 +0530

    some basic modifications

--- ep 01 - Inception/app.js
+++ ep 01 - Inception/app.js
@@ -13,6 +13,7 @@
 //      </div>
 //</div>
 
+//React Create Element -- React object -- HTML Browser Render
 const parent = React.createElement("div",{id: "parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"i'm an h1 tag"),React.createElement("h2",{},"i'm an h2 tag")]));
 //this way of creating element is tedious and messy and confusing .this is why JSX exsists.
 
